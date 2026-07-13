2e98d16746fdc1ec0f9a91a8e588305466c2c20c
Muhammad Maviya Khan <maviya.khan2008@gmail.com>
2026-07-13 13:46:05 +0500

    Add deployment configuration

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,3 @@ import os
 from app import create_app
 
 app = create_app(os.environ.get("FLASK_CONFIG", "development"))
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
